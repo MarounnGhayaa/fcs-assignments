87052b44e69bcd4fa4e73b1c37b38a3cf56a446f
MarounnGhayaa <marounghaya@icloud.com>
2025-02-13 17:50:57 +0200

    exercise 3 <> login system conditions

--- assignment1.py
+++ assignment1.py
@@ -15,3 +15,11 @@ if number % 2 == 0:
     print("The number is even.")
 else:
     print("The number is odd.")
+
+#Exercise 3
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "admin" and password == "1234":
+    print("Access granted.")
+else:
+    print("Access denied.")
